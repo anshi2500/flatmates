7c5792f763164ad464b4dfd2607118e4818abda0
Bradley Li <jizhe.li@ucalgary.ca>
2024-11-17 23:38:22 -0700

    The data retrieval for the edit profile page needs further development

---
+++ FlatMate/Components/GenderPicker.swift
@@ -0,0 +1,18 @@
+//
+//  GenderPicker.swift
+//  FlatMate
+//
+//  Created by 李吉喆 on 2024-11-17.
+//
+
+import SwiftUI
+
+struct GenderPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenderPicker()
+}
---
+++ FlatMate/Components/ProfileField.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileField.swift
+//  FlatMate
+//
+//  Created by 李吉喆 on 2024-11-17.
+//
+
+import SwiftUI
+
+struct ProfileField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileField()
+}
